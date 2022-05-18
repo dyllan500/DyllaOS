fffe400367183060ab2d7355ce3a5af08aa225e3
Dyllan Kobal <dddk500@gmail.com>
2022-05-17 23:47:11 -0400

    Added printf function that prints the the serial console

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,29 @@
 #include <mini_uart.h>
+#include <printf.h>
+
+void putc(void *p, char c) {
+    if (c == '\n') {
+        uart_send('\r');
+    }
+
+    uart_send(c);
+}
+
 
 static unsigned int processor = 0; // global variable to sync the processors
 
 void kernel_main(char proc_id)
 {
     while (processor != proc_id) {} // wait to execute
+    if(proc_id == 0)
+    {
+        uart_init();
+        init_printf(0, putc);
+        printf("\nDyllaOS Initializing...\n");
+    }
     
     
-    uart_send_string("Hello World!!!\n");
+    printf("Hello World!!!!\n");
     
     processor++; // increment 'processor' to enable the next core to execute
     while (1) {
