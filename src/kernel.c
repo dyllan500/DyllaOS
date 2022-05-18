595f6ad9c07a755e3f4b51795bcf4e4f2b18686e
Dyllan Kobal <dddk500@gmail.com>
2022-05-17 23:44:27 -0400

    Added support for uart for serial communitcation with the pi. All four cores will echo serial input back

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,16 @@
+#include <mini_uart.h>
+
 static unsigned int processor = 0; // global variable to sync the processors
 
 void kernel_main(char proc_id)
 {
     while (processor != proc_id) {} // wait to execute
-
+    
+    
+    uart_send_string("Hello World!!!\n");
+    
     processor++; // increment 'processor' to enable the next core to execute
     while (1) {
+        uart_send(uart_recv());
     }
 }
